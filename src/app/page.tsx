e90627dd6f69200fa5d2bca3bf2129fb4a3bfd13
Ethan Konkolowicz <ethan.konkolowicz@gmail.com>
2025-01-08 09:47:03 -0500

    added beginning to token list component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useMemo } from "react";
+import React, { useMemo, useState } from "react";
 import {
 	ConnectionProvider,
 	WalletProvider,
@@ -14,18 +14,21 @@ import {
 import Message from "@/components/Message";
 import WalletBalance from "@/components/WalletBalance";
 import ChartWidget from "@/components/ChartWidget";
+import TokenList, { TokenProps } from "@/components/TokenList";
 import "@/styles/solana-ui.css";
+import { json } from "stream/consumers";
 
-const API_URL =
-	process.env.NEXT_PUBLIC_API_URL ?? "https://api.devnet.solana.com";
-const API_KEY = process.env.NEXT_PUBLIC_API_KEY ?? "";
+const API_SOL_NETWORK_URL =
+	process.env.NEXT_PUBLIC_API_SOL_NETWORK_URL ??
+	"https://api.devnet.solana.com";
+const API_SOL_NETWORK_KEY = process.env.NEXT_PUBLIC_API_SOL_NETWORK_KEY ?? "";
 
 export default function Home() {
 	// The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
 	const network = WalletAdapterNetwork.Devnet;
 	// You can also provide a custom RPC endpoint.
 	// const endpoint = useMemo(() => clusterApiUrl(network), [network]);
-	const endpoint = API_URL + API_KEY;
+	const endpoint = API_SOL_NETWORK_URL + API_SOL_NETWORK_KEY;
 
 	const wallets = useMemo(
 		() => [
@@ -34,6 +37,13 @@ export default function Home() {
 		],
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 		[network]
+	);
+
+	const [sellToken, setSellToken] = useState<TokenProps | null>(
+		JSON.parse(localStorage.getItem("sellToken") ?? "")
+	);
+	const [buyToken, setBuyToken] = useState<TokenProps | null>(
+		JSON.parse(localStorage.getItem("buyToken") ?? "")
 	);
 
 	return (
